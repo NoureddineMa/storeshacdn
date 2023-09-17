2de70ea74d3fbd202b2595d2e200bc998f9ad5bc
NoureddineMa <noureddinemaher13@gmail.com>
2023-09-17 15:31:37 +0200

    initial commit

---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="grid place-items-center h-screen">
+          Next Js iS POWERFULL  
+    </main>
+  )
+}
